720cc0be2dfb475eef435bf47c56e3cf3eab907d
hyunseok <prking94@naver.com>
2023-12-13 16:54:45 +0900

    2023-12-13 board-front LoginUser 관련 인터페이스 추가

--- board-front/src/types/interface/index.ts
+++ board-front/src/types/interface/index.ts
@@ -1,9 +1,10 @@
 import BoardListItem from './board-list-item';
 import CommentListItem from './comment-list-item';
 import FavoriteListItem from './favorite-list-item';
-
+import User from './user.interface';
 export type {
     BoardListItem,
     CommentListItem,
-    FavoriteListItem
+    FavoriteListItem,
+    User
 }
---
+++ board-front/src/types/interface/user.interface.ts
@@ -0,0 +1,6 @@
+export default interface User {
+    email : string,
+    nickName : string,
+    profileImage : string | null
+
+}
