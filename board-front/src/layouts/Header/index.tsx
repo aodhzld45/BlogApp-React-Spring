a710de738ca8d3be916c332bc06fdc54b20014c3
hyunseok <prking94@naver.com>
2023-12-13 15:49:10 +0900

    2023-12-13 board-front Header Layout 및 검색관련 추가

--- board-front/src/layouts/Header/index.tsx
+++ board-front/src/layouts/Header/index.tsx
@@ -1,11 +1,97 @@
-import React from 'react'
 import './style.css'
+import { useNavigate, useParams } from 'react-router-dom'
+import { MAIN_PATH, SEARCH_PATH } from 'constant';
+import { useState, ChangeEvent,KeyboardEvent, useRef, useEffect } from 'react';
 
 // Component : 헤더 화면 (Header) 컴포넌트
 export default function Header() {
 
+// function : 네비게이트 함수 
+const navigate = useNavigate();
+
+// event handler : 로고 클릭 이벤트 함수
+const onLogoClickHandler = () => {
+  navigate(MAIN_PATH());
+}
+
+// Component : 검색 버튼 컴포넌트
+const SearchButton = () => {
+
+// state : 검색어 버튼 요소 참조 상태값 고나리
+const searchButtonRef = useRef<HTMLDivElement | null>(null);
+
+// State : 검색 버튼 상태값 관리
+const [searchStatus, setSearchStatus] = useState<boolean>(false);
+// State : 검색어 상태값 관리
+const [word, setWord] = useState('');
+// State : 검색어 PathVariable 상태값 관리 -> url에 태워보낼 파라미터 이름과 같아야함 searchWord
+const { searchWord } = useParams();
+
+// event handler : 검색 버튼 클릭 이벤트 처리 함수
+const onSearchButtonClickHandler = () => {
+  if (!searchStatus) {
+    setSearchStatus(!searchStatus);
+    return;
+  }
+  navigate(SEARCH_PATH(word));
+}
+
+// event handler : 검색어 변경 이벤트 처리 함수
+const onSearchWordChangHandler = (e :ChangeEvent<HTMLInputElement>) => {
+  const value = e.target.value;
+  setWord(value);
+}
+
+// event handler : 검색어 키 이벤트 처리 함수
+const onSearchWordkeyDownHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+  if (e.key !== 'Enter') return;
+  if (!searchButtonRef.current) return;
+  searchButtonRef.current.click();
+}
+
+// effect : 검색어 Path Variable 변경할 때 마다 실행할 함수
+useEffect(() => {
+  // 만약 검색어가 존재한다면
+  if(searchWord) {
+    // 사용자에게 입력된 HTMLInputElement(word) 검색어에 파라미터값인 searchWord를 넣어주고,
+    setWord(searchWord);
+    // setSearchStatus(true) 값으로 검색 화면으로 이동시 그대로 검색된 검색어를 유지 
+    setSearchStatus(true);
+  }
+}, [searchWord]);
+
+
+// Render : 검색 아이콘 화면 (SearchButton) 렌더링 (클릭 false 상태)
+if(!searchStatus)
+  return (
+  <div className='icon-button' onClick={onSearchButtonClickHandler}>
+    <div className='icon search-light-icon'></div>
+  </div>);
+// Render : 검색 아이콘 화면 (SearchButton) 렌더링 (클릭 true 상태)
+  return (
+    <div className='header-search-input-box'>
+      <input className='header-search-input' type='text' placeholder='검색어를 입력해주세요.' onChange={onSearchWordChangHandler} onKeyDown={onSearchWordkeyDownHandler}/>
+      <div className='icon-button' onClick={onSearchButtonClickHandler}> 
+        <div className='icon search-light-icon'></div>
+      </div>
+    </div>
+  );
+}
+
 // Render : 헤더 화면 (Header) 렌더링 부분
   return (
-    <div>Header 부분</div>
+    <div id='header'>
+      <div className='header-container'>
+        <div className='header-left-box' onClick={onLogoClickHandler}>
+          <div className='icon-box'>
+            <div className='icon log-dark-icon'></div>
+          </div>
+          <div className='header-logo'>{`Seo's Board`}</div>
+        </div>
+        <div className='header-light-box'>
+          <SearchButton />
+        </div>
+      </div>
+    </div>
   )
 }
