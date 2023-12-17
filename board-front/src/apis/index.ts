9d49a63f4f259cad37c5a5fc3f427aa53244362a
hyunseok <prking94@naver.com>
2023-12-17 23:12:23 +0900

    2023-12-17 board-front apis Domain 및 기본 작업 추가

--- board-front/src/apis/index.ts
+++ board-front/src/apis/index.ts
@@ -1,1 +1,16 @@
-export const tmp = "";
+import { SignInRequestDTO, SignUpRequestDTO } from "./request/auth";
+
+const DOMAIN = 'http://localhost:4000';
+
+const API_DOMAIN = `${DOMAIN}/api/v1/`;
+
+const SIGN_IN_URL = () => `${DOMAIN}/api/v1/sign-in`;
+const SIGN_UP_URL = () => `${DOMAIN}/api/v1/sign-up`;
+
+export const signInRequest = (requestBody : SignInRequestDTO)  => {
+
+}
+
+export const signUpRequest = (requestBody : SignUpRequestDTO)  => {
+
+}
---
+++ board-front/src/apis/request/auth/sign-in.request.dto.ts
@@ -0,0 +1,5 @@
+export default interface SignInRequestDTO {
+    // 회원가입 관련 유저 필드 정의
+    email : string;
+    password : string;
+}
