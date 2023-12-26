8720bda7050f8224d535451aa78f2c3ded005bcf
hyunseok <prking94@naver.com>
2023-12-26 22:06:00 +0900

    2023-12-26 board-front board/Write 게시물 작성 페이지 기본 구조 추가

--- board-front/src/views/Board/Write/index.tsx
+++ board-front/src/views/Board/Write/index.tsx
@@ -1,12 +1,60 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './style.css';
+import { useBoardStore } from 'stores';
 
 // Component : 게시물 작성 화면 (BoardWrite) 컴포넌트
 export default function BoardWrite() {
 
-// Render : 게시물 작성 화면 (BoardWrite) 렌더링 부분
+// State : 본문 영역 요소 상태 관리 
+const contentRef = useRef<HTMLTextAreaElement | null>(null);
+// State : 이미지 입력 요소 참조 상태 관리
+const imageInputRef = useRef<HTMLInputElement | null>(null);
 
+// State : 게시물 상태 관리
+const {title, setTitle} = useBoardStore(); 
+const {content, setContent} = useBoardStore();
+const {boardImageFileList, setBoardImageFileList} = useBoardStore();
+const {resetBoard} = useBoardStore(); 
+
+// State : 게시물 이미지 미리보기 URL 상태
+const [imageUrls, setImageUrls] = useState<String[]>([]);
+
+// Effect : 첫 렌더 마운트시 실행할 함수
+useEffect(() => {
+  resetBoard();
+}, []);
+
+
+
+// Render : 게시물 작성 화면 (BoardWrite) 렌더링 부분
   return (
-    <div>게시물 작성 화면 (BoardWrite)</div>
+    <div id='board-write-wrapper'>
+      <div className='board-write-container'>
+        <div className='write-box'>
+          <div className='board-write-title-box'>
+            <input className='board-write-title-input' type='text' placeholder='제목을 작성해주세요.' value={title} />
+          </div>
+
+          <div className='divider'></div>
+
+          <div className='board-write-content-box'>
+            <textarea ref={contentRef} className='board-write-content-textarea'  placeholder='본문을 작성해주세요.' value={content} />
+            <div className='icon-button'>
+              <div className='icon image-box-light-icon'></div>
+            </div>
+            <input ref={imageInputRef} type='file' accept='image/*' style={{"display": "none"}}/>
+          </div>
+         <div className='board-write-images-box'>
+          {/* 이미지를 저장할 박스들 */}
+          <div className='board-write-image-box'>
+            <img className='board-write-image' />
+            <div className='icon-button image-close'>
+              <div className='icon close-icon'></div>
+            </div>
+          </div>
+         </div>
+        </div>
+      </div>
+    </div>
   )
 }
